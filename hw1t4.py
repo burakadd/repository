7465ec275954e6ca6c0820089930cc4217aab179
burakadd <50999098+burakadd@users.noreply.github.com>
2019-06-03 22:46:56 +0300

    hw1t4_done_rev1

--- hw1t4.py
+++ hw1t4.py
@@ -1,12 +1,15 @@
 from math import factorial
 
+
 def cos_tailor_member(i, _arg):
-    return (-1**i)*(_arg**(2*i))/factorial(2*i)
-    
+    return (-1 ** i) * (_arg ** (2 * i)) / factorial(2 * i)
+
+
 def cos_into_tailor(n, _arg):
-    return 1 - sum(map(lambda x: cos_tailor_member(i, _arg), range(n)))
-    
-N = int(input("n = ")
-_argument = int(input("x = ")
+    return 1 + sum(map(lambda n: cos_tailor_member(n, _arg), range(n)))
+
+
+N = int(input("n = "))
+_argument = float(input("x = "))
 
 print(cos_into_tailor(N, _argument))
