51a60a9ef2153c81d977e9ee3998a1ee2f05a34d
anotherbigup <burakad@inbox.ru>
2019-06-10 00:13:25 +0300

    hw2t1_done

--- hw2t1.py
+++ hw2t1.py
@@ -1,6 +1,4 @@
 import calendar
 
-_dict = {0: "Monday", 1: "Tuesday", 2: "Wednesday", 3: "Thursday", 4: "Friday", 5: "Saturday", 6: "Sunday"}
-
 day, month, year = map(int, input('dd.mm.yyyy').split('.'))
-print(_dict.get(calendar.weekday(year, month, day)))
+print(calendar.day_name[calendar.weekday(year, month, day)])
