7b5b688174d2ff2ef9a266449a996dd238f97738
burakadd <50999098+burakadd@users.noreply.github.com>
2019-06-14 17:39:12 +0300

    Update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,8 +1,26 @@
 import typing
-_c = "X"
-_n = "O"
-_list = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+#_c = 
+#_n = "O"
+_list = [[" ", " ", " "], 
+[ " ", " ", " "], 
+[" ", " ", " "]]
 
+pl1 = input("Name of first player is ")
+pl2 = input("Name of second player is ")
+while pl1 == pl2:
+    print("It is name of first player")
+    pl2 = input("Name of second player is ")
+
+
+_dict = {pl1: "X", pl2: "O"} 
+
+
+def _player(_arg):
+    if _arg % 2:
+        return pl1
+    else:
+        return pl2 
+        
 
 def print_grid(_arg):
     _e0 = '      1   2   3'
@@ -12,14 +30,41 @@ def print_grid(_arg):
     l3 = ' 3 | {} | {} | {} |'.format(_arg[2][0], _arg[2][1], _arg[2][2])
     print(_e0, '\n', _e, '\n', l1, '\n', _e, '\n', l2, '\n', _e, '\n', l3, '\n', _e)
 
+print_grid(_list)
 
 def make_turn(_arg1):
-    print("It's YOUR turn")
-    abscisse = int(input("ROW number = ")) - 1
-    ordinatus = int(input("COLUMN number = ")) - 1
-    _arg1[abscisse][ordinatus] = 1
+    while True:
+        abscisse, ordinatus = -1, -1
+        print("It is {}'s turn".format(_player(turn_number)))
+        print("Select row's number from set {1, 2, 3}")
+        while abscisse < 0 or abscisse > 2:
+            try:
+                abscisse = int(input("ROW number = ")) - 1                     
+            except ValueError:
+                print("FAIL!!! ENTER CORRECTLY")
+        print("Select column's number from set {1, 2, 3}")
+        while ordinatus < 0 or ordinatus > 2:
+            try:
+                ordinatus = int(input("COLUMN number = ")) - 1                     
+            except ValueError:
+                print("FAIL!!! ENTER CORRECTLY")
+        if _arg1[abscisse][ordinatus] != "X" and _arg1[abscisse][ordinatus] != "O":
+           _arg1[abscisse][ordinatus] = _dict.get(_player(turn_number))
+           break
+        else:
+            print("WRONG TURN!!! THERE IS ANOTHER SYMBOL IN YHIS CELL")
+
+def check_win(_arg):
+    for i in _arg:
+        if len(set(i)) == 1:
+            print("{} IS WINNER".format(_player(turn_number))
+print("LET'S PLAY")
+turn_number = 1   
+for i in range(3):
+    make_turn(_list)
+    turn_number += 1
+    print_grid(_list)
 
 
-print_grid(_list)
-make_turn(_list)
-print_grid(_list)
+
+
