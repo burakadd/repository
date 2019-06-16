c9f584f78e72d9836f0d2b6e0c15afc582043f08
anothrbigup <burakad@inbox.ru>
2019-06-17 02:00:48 +0300

    almost done

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -16,11 +16,11 @@ def print_grid(_arg):
 
 def make_turn(_arg1, _arg2, _arg3, _arg4):
     _dict = {1: "X", 0: "O"}
+    if _arg2 < 0 or _arg2 > 2 or _arg3 < 0 or _arg3 > 2:
+        print("YOU MUST ENTER VALUE FROM SET {1, 2, 3}")
+        return False
     if _arg1[_arg2][_arg3] == "X" or _arg1[_arg2][_arg3] == "O":
         print("WRONG TURN!!! THERE IS ANOTHER SYMBOL IN THIS CELL")
-        return False
-    elif _arg2 < 0 or _arg2 > 2 or _arg3 < 0 or _arg3 > 2:
-        print("WRONG TURN!!! YOU MUST ENTER VALUE FROM SET {1, 2, 3}")
         return False
     _arg1[_arg2][_arg3] = _dict.get(_arg4 % 2)
     return True
