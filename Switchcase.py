4c5295d4c7a925407df3fd2a5c84e0d550d7ae8c
burakadd <50999098+burakadd@users.noreply.github.com>
2019-06-27 17:14:41 +0300

    Switchcase_rev2_done

--- Switchcase.py
+++ Switchcase.py
@@ -1,44 +1,36 @@
 import random
 import functools
 
-def _join(_arg):
-    _list = [i*10**(len(_arg) - j) for j, i in enumerate(_arg, 1)]
-    return functools.reduce(lambda i,j: i + j, _list)
-
 
 def _prime(_arg):
-    if _arg % 2 == 0:
-        return False
-    d = 3
-    while d**2 <= _arg and _arg % d != 0:
-        d += 2
-    if d**2 > _arg:
-        return True
+    if _arg % 2:
+        d = 3
+        while d**2 <= _arg and _arg % d:
+            d += 2
+        if d**2 > _arg:
+            return True
     else:
         return False
+   
 
+reducers = {"sum": lambda i, j: i + j, 
+    "multiply": lambda i, j: i * j,
+    "join": lambda i, j: 10 * i + j, 
+    "union": lambda i, j: [*i, j] if not j in i else i,
+    "reversed": lambda i, j: [j, *i]}
+mappers = {"negated": lambda i: -i, 
+    "inverted": lambda i: i ** -1, 
+    "squared": lambda i: i ** 2}
+filters = {"odds": lambda i: i % 2, 
+    "evens": lambda i: not i % 2, 
+    "simples": lambda i: _prime(i)}
+reducerinitial = {"sum": 0, 
+    "multiply": 1,
+    "join": 0,
+    "union": [],
+    "reversed": []}
 
-def _switchcase(_arg, *funcs):
-    _switchcaselist = {"sum": functools.reduce(lambda i, j: i + j, _arg),
-             "multiply": functools.reduce(lambda i, j: i * j, _arg),
-             "join": _join(_arg),
-             "union": set(_arg),
-             "reversed": reversed(_arg),
-
-             "negated": list(map(lambda i: -i, _arg)),
-             "inverted": list(map(lambda i: i**(-1), _arg)),
-             "squared": list(map(lambda i: i**2, _arg)),
-
-             "odds": list(filter(lambda i: i % 2 == 1, _arg)),
-             "evens": list(filter(lambda i: i % 2 == 0, _arg)),
-             "simples": list(filter(lambda i: _prime(i), _arg))}
-    return _switchcaselist[func]
-
-
-n = int(input("n = "))    
-_data = [random.randint(1, 9) for i in range(n)]
+_data = [random.randint(1, 9) for i in range(int(input("n = ")))]
 print(_data)
-for func in reversed(input("input funcs ").split(" ")):
-    print(func)
-    _data = _switchcase(_data, func)
-    print(_data)
+_reducer, _mapper, _filter = input("input funcs").split(" ")
+print(functools.reduce(reducers[_reducer], map(mappers[_mapper], filter(filters[_filter], _data)), reducerinitial[_reducer]))
