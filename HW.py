02f9da5df41801d8e26bd2ffe4bdd6189fdcc50c
Guck111 <7080446@gmail.com>
2019-07-09 18:49:01 +0300

    update_ttt

--- HW.py
+++ HW.py
@@ -1,23 +1,46 @@
-from socket import create_connection, SHUT_WR, socket as so, getaddrinfo as ge
+# from socket import create_connection, SHUT_WR, socket, getaddrinfo
+# #
+# # with create_connection(('google.by', 80)) as s:
+# #     s.send(b'GET / HTTP/1.1\r\n\r\n')
+# #     s.shutdown(SHUT_WR)
+# #     data = s.recv(100000)
+# #     print(data.decode())
+#
+# family, type, proto, name, addr = getaddrinfo("127.0.0.1", 12345)[0]
+# server = socket(family, type, proto)
+# server.bind(addr)
+# server.listen(5)
+#
+#
+#
+#
+# client1 = create_connection(('127.0.0.1', 12345))
+# client2, addr = server.accept()
+#
+# client2.send(b'GET HTTP/1.0 404 OK')
+# client2.shutdown(SHUT_WR)
+#
+# data = client1.recv(1000)
+# print(data)
+#
+# client2.close()
+# client1.close()
+import socket
 
-# with create_connection(('google.by', 80)) as s:
-#     s.send(b'GET / HTTP/1.1\r\n\r\n')
-#     s.shutdown(SHUT_WR)
-#     data = s.recv(100000)
-#     print(data.decode())
 
-family, type, proto, name, addr = ge("127.0.0.1", 1234)[0]
-server = so(family, type, proto)
-server.bind(addr)
-server.listen(5)
-client1 = create_connection(('127.0.0.1', 1234))
-client2, addr = server.accept()
+sock = socket.socket
+sock.bind(("", 12345))
+sock.listen(8)
 
-client2.send(b'Hello!')
-client2.shutdown(SHUT_WR)
+conn, addr = sock.accept()
 
-data = client1.recv(1000)
-print(data)
+print('connected:', addr)
 
-client2.close()
-client1.close()
+while True:
+    data = conn.recv(1024)
+    if not data:
+        break
+    conn.send(data.upper())
+
+conn.close()
+
--- Telephone number.py
+++ Telephone number.py
@@ -2,12 +2,11 @@ import re
 
 
 def create_phone_number(n):
-    return f"""({
-re.findall(r'^[0-9]{3}', "".join(map(str, n)))[0]}) {
-re.findall(r'.{3}([0-9]{3})', "".join(map(str, n)))[0]}-{
-re.findall(r'[0-9]{4}$', "".join(map(str, n)))[0]}"""
+    a = "".join(map(str, n))
+    return f'({a[:3]}) {a[3:6]}-{a[:-1]}'
 
 
-print(create_phone_number(list(range(10))))
-
-re.sub(
+#     return f"""({
+# re.findall(r'^.{3}', "".join(map(str, n)))[0]}) {
+# re.findall(r'.{3}(.{3})', "".join(map(str, n)))[0]}-{
+# re.findall(r'.{4}$', "".join(map(str, n)))[0]}"""
